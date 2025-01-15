f81a32b1fc29e753b4c8c87c858e35bbd25ddc2d
NabilNugraha17 <nabilnugraha1717@gmail.com>
2025-01-15 09:25:05 +0800

    a

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <AuthenticatedLayout
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800">
-                    Dashboard tegar eka
+                    Dashboard tegar eka pratama
                 </h2>
             }
         >
